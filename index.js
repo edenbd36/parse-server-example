7ef4d0e8d8a7f02c4fd8079906ab946cfb65088b
Eden Ben David <edenbd@gmail.com>
2018-01-10 16:30:57 +0200

    fixed bug

--- index.js
+++ index.js
@@ -26,9 +26,6 @@ var api = new ParseServer({
         apiKey: 'AIzaSyB-U6WXWje2hTokIxSyyuy8j-g0MBvaDm4' // The Server API Key of GCM
     }
   }
-  liveQuery: {
-    classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
-  }
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
